aa36f270d6ffd3664991b39b2c512d7d7670c1b5
asvn9 <128585455+asvn9@users.noreply.github.com>
2023-04-20 18:35:46 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-if (typeof document !== 'undefined') {
 ReactDOM.render(<App />, document.getElementById('app'))
-}
+
