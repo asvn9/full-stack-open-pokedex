8d0627f5f22cec684232dcb8fef2d89426f890c2
Anssi <asuv90@gmail.com>
2023-04-21 18:15:17 +0300

    added test

--- cypress/e2e/poke.cy.js
+++ cypress/e2e/poke.cy.js
@@ -4,4 +4,12 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('can navigate to a pokemon page and view its details', function(){
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+
+    cy.contains('ivysaur')
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
