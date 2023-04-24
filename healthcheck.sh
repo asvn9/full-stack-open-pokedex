5c715f30717f5956bcd82c49938c698b4ce41f08
Anssi <asuv90@gmail.com>
2023-04-24 17:54:32 +0300

    final healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+response=$(curl -s https://asvnpokedex.fly.dev/health)
 
-exit 0 
+if [ "$response" = "ok" ]; then
+  echo "Health check successful"
+  exit 0
+else
+  echo "Health check failed"
+  exit 1
+fi
